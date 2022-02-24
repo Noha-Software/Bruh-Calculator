355082c36cee9b6bd43ffd32784639c7f1bc99b1
Gyász <engineeradam95@gmail.com>
2022-02-24 11:39:56 +0100

    Added UI for pages

--- Assets/Scripts/RemarkableIdentities.cs
+++ Assets/Scripts/RemarkableIdentities.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RemarkableIdentities : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ClosePage()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject currentPageOpen = GameObject.Find("Tab 3 - Remarkable Identities/Tabs").GetComponent<TabGroup>().currentPageOpen;
+        currentPageOpen.SetActive(false);
     }
 }
