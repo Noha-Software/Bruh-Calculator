adf01336b3767be940289dc8c295fce1988b8e4e
Gyász <engineeradam95@gmail.com>
2022-02-26 20:37:07 +0100

    Begined scripting

--- Assets/Scripts/RemarkableIdentities.cs
+++ Assets/Scripts/RemarkableIdentities.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RemarkableIdentities : MonoBehaviour
 {
+    public string aInputField;
+    public string bINputField;
+
+    public void GetSliceInputs()
+    {
+        string aInputName = GameObject.Find("Tab 3 - Remarkable Identities/Tabs").GetComponent<TabGroup>().currentPageOpen.name + "/aInput";
+        string bInputName = GameObject.Find("Tab 3 - Remarkable Identities/Tabs").GetComponent<TabGroup>().currentPageOpen.name + "/bInput";
+        aInputField = GameObject.Find(aInputName).GetComponent<TMP_InputField>().text;
+        bINputField = GameObject.Find(bInputName).GetComponent<TMP_InputField>().text;
+        Debug.Log(aInputField + bINputField);
+
+    }
+
     public void ClosePage()
     {
         GameObject currentPageOpen = GameObject.Find("Tab 3 - Remarkable Identities/Tabs").GetComponent<TabGroup>().currentPageOpen;
