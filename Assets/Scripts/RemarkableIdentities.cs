3a5b1394f559ccb9e9e04374f370ec45c5ed6b42
Gyász <engineeradam95@gmail.com>
2022-02-27 14:04:51 +0100

    Scripting Remarkable Identities #1

--- Assets/Scripts/RemarkableIdentities.cs
+++ Assets/Scripts/RemarkableIdentities.cs
@@ -5,17 +5,71 @@ using TMPro;
 
 public class RemarkableIdentities : MonoBehaviour
 {
-    public string aInputField;
-    public string bINputField;
+    bool isNumberOfA;
+    bool isVariableOfA;
+    bool isNumberOfB;
+    bool isVariableOfB;
 
-    public void GetSliceInputs()
+    public string aInputField;
+    public string bInputField;
+
+    List<string> aNumbers;
+    List<string> aVariables;
+    List<string> bNumbers;
+    List<string> bVariables;
+
+    private void Start()
     {
+        aNumbers = new List<string>();
+    }
+    public void GetInputs()
+    {
+        //Get the inputs as strings
         string aInputName = GameObject.Find("Tab 3 - Remarkable Identities/Tabs").GetComponent<TabGroup>().currentPageOpen.name + "/aInput";
         string bInputName = GameObject.Find("Tab 3 - Remarkable Identities/Tabs").GetComponent<TabGroup>().currentPageOpen.name + "/bInput";
         aInputField = GameObject.Find(aInputName).GetComponent<TMP_InputField>().text;
-        bINputField = GameObject.Find(bInputName).GetComponent<TMP_InputField>().text;
-        Debug.Log(aInputField + bINputField);
+        bInputField = GameObject.Find(bInputName).GetComponent<TMP_InputField>().text;
+        //Debug.Log(aInputField + bINputField);
 
+    }
+    public void SortData()
+    {
+        GetInputs();
+        //Send the inputs to their respectable lists
+        //Get the numbers of 'a'
+        int idxTracker = 0;
+        string currentString = "";
+
+        foreach(char c in aInputField)
+        {
+            if((int)c <= 57 && (int)c >= 48 && idxTracker < 2)
+            {
+                isNumberOfA = true;
+                currentString += c;
+                Debug.Log(idxTracker);
+            }
+            else if ((int)c == 94)
+            {
+                //debug needed
+                currentString += c;
+                ++idxTracker;
+                Debug.Log(idxTracker);
+            }
+            if (idxTracker > 1)
+            {
+                aNumbers.Add(currentString);                
+                Debug.Log("Is number of a? : " + isNumberOfA);
+                Debug.Log("Is varuable of a? : " + isVariableOfA);
+                Debug.Log("is number of b? : " + isNumberOfB);
+                Debug.Log("Is variable of b? : " + isVariableOfB);
+                foreach (string s in aNumbers)
+                {
+                    Debug.Log("Index" + aNumbers.IndexOf(s) + "of aNumbers : " + s);
+                }
+                currentString = "";
+                idxTracker = 0;
+            }                                                     
+        }
     }
 
     public void ClosePage()
