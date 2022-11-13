e2337ac2e8a15b23e16b2654d9a1b36de9e6ac7b
Kevlaris <team3csh@gmail.com>
2022-11-13 17:27:01 +0100

    About information

---
+++ Assets/Scripts/InfoText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using TMPro;
+
+#if UNITY_EDITOR
+
+[ExecuteInEditMode]
+public class InfoText : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI infoText;
+
+	private void Reset()
+	{
+		infoText.text = GetInformation();
+	}
+	private void OnEnable()
+	{
+		infoText.text = GetInformation();
+	}
+	private void Start()
+	{
+		infoText.text = GetInformation();
+	}
+
+	string GetInformation()
+	{
+		int year = System.DateTime.Now.Year;
+		string version = PlayerSettings.bundleVersion;
+		string company = PlayerSettings.companyName;
+		return "© " + year + ' ' + company + '\n' + version;
+	}
+
+	public void GitHubLink()
+	{
+		Debug.Log("Opening link...");
+		Application.OpenURL("https://github.com/Noha-Software");
+	}
+	public void EmailLink()
+	{
+		Debug.Log("Opening link...");
+		Application.OpenURL("mailto:noha.software@gmail.com");
+	}
+}
+#endif
